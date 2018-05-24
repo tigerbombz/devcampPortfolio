c2ad66b2dd1c376a6d51fa67a5d5e20f7ece7a5d
Matthew <madd1778@yahoo.com>
2018-05-24 15:27:31 -0700

    implemented blogs controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,12 @@ Rails.application.routes.draw do
 
 
 
-  resources :blogs
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
