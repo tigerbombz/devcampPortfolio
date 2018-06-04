0a4abbf0927213d94551b2a6368731cb0eda3eae
Matthew <madd1778@yahoo.com>
2018-06-04 10:47:51 -0700

    added basic authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :portfolios, except: [:show]
-
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
-
-
 
   resources :blogs do
     member do
