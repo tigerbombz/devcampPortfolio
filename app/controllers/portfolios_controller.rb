c671702babce341093a9c432198031796c34b84a
Matthew <madd1778@yahoo.com>
2018-06-04 14:30:50 -0700

    added custom layouts for blogs and portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,6 @@
 class PortfoliosController < ApplicationController
+  layout "portfolio"
+  
   def index
     # @portfolio_items = Portfolio.ruby_on_rails_portfolio_items
     @portfolio_items = Portfolio.all
