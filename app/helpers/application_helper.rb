407c1c5c2084aacf2ff263fe71a48b8b9fd14f6a
Matthew <madd1778@yahoo.com>
2018-06-15 12:04:36 -0700

    changed header styling and tweet stlying

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -37,6 +37,10 @@ module ApplicationHelper
       {
         url: portfolios_path,
         title: 'Portfolio'
+      },
+      {
+        url: tech_news_path,
+        title: 'Tech News'
       }
     ]
   end
